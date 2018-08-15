c1ae9f0ac865501468bf655fd5f6c60850ee3b4c
yhazir2017 <yildiray.hazir@gmail.com>
2018-08-15 12:07:07 -0700

    changed

--- GitHupTest/GitHupTest/GitHupTest/ViewController.swift
+++ GitHupTest/GitHupTest/GitHupTest/ViewController.swift
@@ -1,9 +1,9 @@
 //
 //  ViewController.swift
-//  Test
+//  GitHupTest
 //
 //  Created by YILDIRAY HAZIR on 8/15/18.
-//  Copyright © 2018 YildirayHazir. All rights reserved.
+//  Copyright © 2018 Yildiray Hazir. All rights reserved.
 //
 
 import UIKit
@@ -13,13 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Test for Yildiray Hazir")
+        print("GitHUP Test")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
